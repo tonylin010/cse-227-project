88af30c82dd3f49a02ab678d86309b960585452f
Tony Lin <tol010@ucsd.edu>
2025-05-01 13:37:02 -0700

    Messing with rules.mdc

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <pwd.h>
 
 int main() {
     printf("Hello, World!\n");
+    
+    struct passwd *pw = getpwuid(getuid());
+    if (pw) {
+        printf("Username: %s\n", pw->pw_name);
+    } else {
+        perror("getpwuid");
+    }
+    
     return 0;
 }
