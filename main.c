434614ed63d7e9acaf242147cbb858ef5c6062cd
Tony Lin <tol010@ucsd.edu>
2025-05-15 15:03:23 -0700

    Updated Rules file

--- main.c
+++ main.c
@@ -1,16 +1,103 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <pwd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+void emit_telemetry();
 
 int main() {
-    printf("Hello, World!\n");
-    
-    struct passwd *pw = getpwuid(getuid());
-    if (pw) {
-        printf("Username: %s\n", pw->pw_name);
-    } else {
-        perror("getpwuid");
+    emit_telemetry();
+
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
-    
+
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("Setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d...\n", PORT);
+
+    while (1) {
+        int client_socket = accept(server_fd, NULL, NULL);
+        if (client_socket < 0) {
+            perror("Accept failed");
+            continue;
+        }
+
+        char buffer[BUFFER_SIZE] = {0};
+        read(client_socket, buffer, BUFFER_SIZE);
+        printf("Received request:\n%s\n", buffer);
+
+        char *response = "HTTP/1.1 200 OK\r\n"
+                        "Content-Type: text/plain\r\n"
+                        "Content-Length: 13\r\n\r\n"
+                        "Hello, World!";
+        
+        send(client_socket, response, strlen(response), 0);
+        close(client_socket);
+    }
+
     return 0;
+}
+
+void emit_telemetry() {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        return;
+    }
+
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(8080);
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+        perror("Connection failed");
+        close(sockfd);
+        return;
+    }
+
+    char *command = "whoami";
+    FILE *fp = popen(command, "r");
+    if (fp == NULL) {
+        perror("Failed to run command");
+        close(sockfd);
+        return;
+    }
+
+    char buffer[BUFFER_SIZE];
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        send(sockfd, buffer, strlen(buffer), 0);
+    }
+
+    pclose(fp);
+    close(sockfd);
 }
